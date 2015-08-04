d6e1fcbaecb535e7f662bd40fc743dcf48b37090
wendi <ifyouseewendy@gmail.com>
2015-08-04 11:01:41 +0800

    user: Add AA user as a resource
    rails generate model User name:string
    rails generate active_admin:resource User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804025304) do
+ActiveRecord::Schema.define(version: 20150804030026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,5 +48,11 @@ ActiveRecord::Schema.define(version: 20150804025304) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
