db868277859517979906daa6df26597144b315b5
wendi <ifyouseewendy@gmail.com>
2015-08-05 11:49:56 +0800

    demo: Project index display

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -7,4 +7,31 @@ ActiveAdmin.register Project do
   end
 
   permit_params :name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    actions
+  end
+
+  index as: :grid do |project|
+    div do
+      h3 "#{project.name} - #{project.description}"
+    end
+  end
+
+  index as: :block do |project|
+    div for: project do
+      resource_selection_cell project
+      h2 auto_link project.name
+      div simple_format project.description
+    end
+  end
+
+  index as: :blog do |project|
+    title :name
+    body  :description
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804063931) do
+ActiveRecord::Schema.define(version: 20150805033433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,8 +58,9 @@ ActiveRecord::Schema.define(version: 20150804063931) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   create_table "tickets", force: :cascade do |t|
