b53f159ad61f21d42b7ec49d0c469caf504f9389
wendi <ifyouseewendy@gmail.com>
2015-08-10 11:13:09 +0800

    admin_user: TDD to add role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805033433) do
+ActiveRecord::Schema.define(version: 20150810023118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150805033433) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role",                   default: 0
   end
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
--- test/models/admin_user_test.rb
+++ test/models/admin_user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class AdminUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  self.use_instantiated_fixtures = true
+
+  test "role" do
+    assert @alpha.in_administration?
+
+    @alpha.in_business!
+    assert @alpha.in_business?
+
+    refute @alpha.in_finance?
+  end
 end
