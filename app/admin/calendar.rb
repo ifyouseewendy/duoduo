b0db67ba164a471cf668cd3b1c5dea7cf05e2891
wendi <ifyouseewendy@gmail.com>
2015-08-11 13:20:46 +0800

    AA: Set false to default namespace

--- app/admin/calendar.rb
+++ app/admin/calendar.rb
@@ -16,10 +16,10 @@ ActiveAdmin.register_page "Calendar" do
 
   page_action :add_event, method: :post do
     # ...
-    redirect_to admin_calendar_path, notice: "Your event was added"
+    redirect_to calendar_path, notice: "Your event was added"
   end
 
   action_item :add do
-    link_to "Add Event", admin_calendar_add_event_path, method: :post
+    link_to "Add Event", calendar_add_event_path, method: :post
   end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -9,7 +9,7 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  config.site_title_link = "/admin"
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
@@ -30,7 +30,7 @@ ActiveAdmin.setup do |config|
   # will namespace routes to /hello_world/*
   #
   # To set no namespace by default, use:
-  #   config.default_namespace = false
+  config.default_namespace = false
   #
   # Default:
   # config.default_namespace = :admin
