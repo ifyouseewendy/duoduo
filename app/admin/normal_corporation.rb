6503d5838f511127e8906cebc506305c78ef7594
wendi <ifyouseewendy@gmail.com>
2015-08-11 14:20:44 +0800

    normal corp: Add form

--- app/admin/normal_corporation.rb
+++ app/admin/normal_corporation.rb
@@ -2,18 +2,34 @@ ActiveAdmin.register NormalCorporation do
   menu \
     parent: I18n.t("activerecord.models.corporation")
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  permit_params NormalCorporation.column_names - %w(id created_at updated_at)
 
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs do
+      f.input :name, as: :string
+      f.input :license, as: :string
+      f.input :taxpayer_serial, as: :string
+      f.input :organization_serial, as: :string
+      f.input :corporate_name, as: :string
+      f.input :address, as: :string
+      f.input :account, as: :string
+      f.input :account_bank, as: :string
+      f.input :contact, as: :string
+      f.input :telephone, as: :phone
+      f.input :contract_due_time, as: :datepicker
+      f.input :contract_amount, as: :number
+      f.input :admin_charge_type, as: :radio, collection: Rails.application.secrets.admin_charge_type.map{|n| I18n.t("misc.admin_charge_type.#{n}")}
+      f.input :admin_charge_amount, as: :number
+      f.input :expense_date, as: :datepicker
+      f.input :stuff_count, as: :number
+      f.input :insurance_count, as: :number
+      f.input :jiyi_company_name, as: :radio, collection: Rails.application.secrets.jiyi_company_names
+      f.input :remark, as: :text
+    end
+
+    f.actions
+  end
 
 end
