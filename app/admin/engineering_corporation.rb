840dc53dc5e20634af768ca15dedf5d73785c5dc
wendi <ifyouseewendy@gmail.com>
2015-08-12 10:13:34 +0800

    engi corp: Add form and filter

--- app/admin/engineering_corporation.rb
+++ app/admin/engineering_corporation.rb
@@ -3,18 +3,37 @@ ActiveAdmin.register EngineeringCorporation do
     parent: I18n.t("activerecord.models.corporation"),
     priority: 22
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  permit_params EngineeringCorporation.column_names - %w(id created_at updated_at)
 
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
 
+    f.inputs do
+      f.input :nest_id, as: :string
+      f.input :name, as: :string
+      f.input :start_date, as: :datepicker
+      f.input :project_date, as: :datepicker
+      f.input :project_name, as: :string
+      f.input :project_amount, as: :number
+      f.input :admin_amount, as: :number
+      f.input :total_amount, as: :number
+      f.input :income_date, as: :datepicker
+      f.input :income_amount, as: :number
+      f.input :outcome_date, as: :datepicker
+      f.input :outcome_referee, as: :string
+      f.input :outcome_amount, as: :number
+      f.input :proof, as: :string
+      f.input :actual_project_amount, as: :number
+      f.input :actual_admin_amount, as: :number
+      f.input :already_get_contract, as: :boolean
+      f.input :already_sign_dispatch, as: :boolean
+      f.input :jiyi_company_name, as: :radio, collection: Rails.application.secrets.jiyi_company_names
+      f.input :remark, as: :text
+    end
+
+    f.actions
+  end
+
+  preserve_default_filters!
+  filter :jiyi_company_name, as: :select, collection: proc { Rails.application.secrets.jiyi_company_names }
 end
