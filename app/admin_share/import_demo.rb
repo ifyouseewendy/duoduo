c88dbb092838985bf02f9388e88e6b38cdc92e4b
wendi <ifyouseewendy@gmail.com>
2015-08-16 21:03:36 +0800

    import demo: I18n support on column names

--- app/admin_share/import_demo.rb
+++ app/admin_share/import_demo.rb
@@ -2,15 +2,15 @@ module ImportDemo
   def self.included(base)
     base.instance_eval do
       collection_action :import_demo do
-        model = controller_name.classify
+        model = controller_name.classify.constantize
         data = \
           CSV.generate encoding: 'GBK' do |csv|
             csv << [I18n.t("misc.import_demo.notice")]
-            csv << model.constantize.column_names - %w(id created_at updated_at)
+            csv << (model.column_names - %w(id created_at updated_at)).map{|col| model.human_attribute_name(col) }
           end
         send_data \
           data,
-          :filename => I18n.t("activerecord.models.#{model.underscore}") + " - " + I18n.t("misc.import_demo.name") + '.csv'
+          :filename => I18n.t("activerecord.models.#{model.to_s.underscore}") + " - " + I18n.t("misc.import_demo.name") + '.csv'
       end
     end
   end
