3f8ba8fc8339dee4bcb28db2226205b70cd759ed
wendi <ifyouseewendy@gmail.com>
2015-08-16 21:31:40 +0800

    AA: Set csv encoding to GBK

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -194,6 +194,7 @@ ActiveAdmin.setup do |config|
   #
   # Force the use of quotes
   # config.csv_options = { force_quotes: true }
+  config.csv_options = { encoding: 'GBK' }
 
   # == Menu System
   #
