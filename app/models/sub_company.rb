9b92df5912486ee1252cc7e7b119a07723f805bf
wendi <ifyouseewendy@gmail.com>
2015-08-22 10:51:26 +0800

    sub company: TDD on add_file

--- app/models/sub_company.rb
+++ app/models/sub_company.rb
@@ -3,4 +3,14 @@ class SubCompany < ActiveRecord::Base
   has_and_belongs_to_many :engineering_corporations
 
   mount_uploaders :contracts, ContractUploader
+
+  def add_file(filename, override: false)
+    if override
+      self.contracts = [File.open(filename)]
+    else
+      self.contracts = self.contracts + [File.open(filename)]
+    end
+
+    self.save!
+  end
 end
--- test/models/sub_company_test.rb
+++ test/models/sub_company_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SubCompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @one = sub_companies(:one)
+  end
+
+  def test_add_file
+    assert_empty @one.contracts
+
+    @one.add_file(Rails.root.join('Gemfile'))
+    assert 1, @one.contracts.count
+
+    @one.add_file(Rails.root.join('Rakefile'))
+    assert 2, @one.contracts.count
+
+    @one.add_file(Rails.root.join('config.ru'), override: true)
+    assert 1, @one.contracts.count
+    assert 'config.ru', @one.contracts.first.file.identifier
+  end
 end
