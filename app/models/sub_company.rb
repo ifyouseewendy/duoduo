90d2b46aaa53181662f7dcadd7dd6676564ac0aa
wendi <ifyouseewendy@gmail.com>
2015-08-23 12:05:48 +0800

    sub company: Add generate_docx

--- app/models/sub_company.rb
+++ app/models/sub_company.rb
@@ -15,4 +15,56 @@ class SubCompany < ActiveRecord::Base
 
     self.save!
   end
+
+  def generate_docx(gsub:, file_path:)
+    # Check existance
+    raise "File not exist" unless File.exist?(file_path)
+
+    # Make temp dir
+    temp_path = TempPath.generate
+    puts "--> Generate temp path: #{temp_path}"
+
+    # Make a copy
+    FileUtils.cp file_path, temp_path
+    file = Pathname.new(temp_path).join( Pathname.new(file_path).basename )
+
+    # Extract word/documents
+    Dir.chdir(temp_path)
+    `unzip #{file.basename}`
+
+    # Read content
+    data = File.read("word/document.xml")
+
+    # Substitution
+    gsub.each {|k, v| data.gsub!(k.to_s, v) }
+
+    # Write content
+    File.open("word/document.xml", 'w'){|wf| wf.write data}
+
+    # Zip the docx
+    `zip #{file.basename} word/document.xml`
+
+    # Move to tmp/docx
+    FileUtils.cp file, TempPath.base
+
+    # Return file
+    Pathname.new(TempPath.base.join(file.basename))
+  ensure
+    Dir.chdir(Rails.root)
+    temp_path.rmtree
+  end
+
+  class TempPath
+    class << self
+      def generate
+        dir ||= base.join(SecureRandom.hex(10))
+        dir.mkdir unless dir.exist?
+        dir
+      end
+
+      def base
+        @_base ||= Rails.root.join("tmp").join("docx")
+      end
+    end
+  end
 end
