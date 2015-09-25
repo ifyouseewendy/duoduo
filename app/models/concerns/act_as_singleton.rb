4286af3a966a7b1ff9ebc450f332d55f9ea7520b
wendi <ifyouseewendy@gmail.com>
2015-09-26 00:20:36 +0800

    act_as_singleton: Add module to make base a singleton

---
+++ app/models/concerns/act_as_singleton.rb
@@ -0,0 +1,22 @@
+module ActAsSingleton
+  extend ActiveSupport::Concern
+
+  included do
+    private_class_method :new
+
+    before_create :confirm_singularity
+
+    private
+
+      def confirm_singularity
+        raise "#{self.class} is a Singleton class." if self.class.count > 0
+      end
+  end
+
+  module ClassMethods
+
+    def instance
+      @_instance ||= (self.first || self.create)
+    end
+  end
+end
