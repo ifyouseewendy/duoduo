121aba544edb5260a60fd4fe1c05ec9f784e68d7
wendi <ifyouseewendy@gmail.com>
2015-09-28 16:49:46 +0800

    salary_item: Use revise_total

--- app/models/salary_item.rb
+++ app/models/salary_item.rb
@@ -16,12 +16,24 @@ class SalaryItem < ActiveRecord::Base
       staff = salary_table.normal_corporation.normal_staffs.where(name: name).first
       raise "No staff found for name: #{name}" if staff.nil?
 
-      self.create!( {normal_staff: staff, salary_deserve: salary}.merge( staff.insurance_fund ) )
+      item = self.new( {normal_staff: staff, salary_deserve: salary}.merge( staff.insurance_fund ) )
+      item.revise_total!
+      item
     end
   end
 
-  def total_personal
-    @_total_personal ||= [
+  def revise_total!
+    set_total_personal
+    set_salary_in_fact
+    set_total_company
+    set_admin_amount
+    set_total_sum
+    set_total_sum_with_admin_amount
+    self.save!
+  end
+
+  def set_total_personal
+    self.total_personal = [
       pension_personal, pension_margin_personal,
       unemployment_personal, unemployment_margin_personal,
       medical_personal, medical_margin_personal,
@@ -32,12 +44,12 @@ class SalaryItem < ActiveRecord::Base
     ].map(&:to_f).sum
   end
 
-  def salary_in_fact
-    salary_deserve - total_personal
+  def set_salary_in_fact
+    self.salary_in_fact = salary_deserve - total_personal
   end
 
-  def total_company
-    @_total_company ||= [
+  def set_total_company
+    self.total_company = [
       pension_company, pension_margin_company,
       unemployment_company, unemployment_margin_company,
       medical_company, medical_margin_company,
@@ -48,23 +60,25 @@ class SalaryItem < ActiveRecord::Base
     ].map(&:to_f).sum
   end
 
-  def admin_amount
-    if corporation.by_rate?
-      total_sum * corporation.admin_charge_amount
-    else
-      corporation.admin_charge_amount
-    end
+  def set_admin_amount
+    self.admin_amount = \
+      if corporation.by_rate?
+        set_total_sum * corporation.admin_charge_amount
+      else
+        corporation.admin_charge_amount
+      end
+  end
+
+  def set_total_sum
+    self.total_sum = salary_deserve + total_company
+  end
+
+  def set_total_sum_with_admin_amount
+    self.total_sum_with_admin_amount = total_sum + admin_amount
   end
 
   def corporation
     normal_staff.normal_corporation
   end
 
-  def total_sum
-    salary_deserve + total_company
-  end
-
-  def total_sum_with_admin_amount
-    total_sum + admin_amount
-  end
 end
