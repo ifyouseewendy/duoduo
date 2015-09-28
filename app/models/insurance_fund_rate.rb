819d505bdddb9411bd3df4f2d5be49880878ac8c
wendi <ifyouseewendy@gmail.com>
2015-09-28 14:06:11 +0800

    insurance_fund_rate: Make doubleton

--- app/models/insurance_fund_rate.rb
+++ app/models/insurance_fund_rate.rb
@@ -1,2 +1,8 @@
 class InsuranceFundRate < ActiveRecord::Base
+  include ActAsSingleton
+
+  # The model contains only 2 records, maybe called Dounbleton
+  def confirm_singularity
+    raise "#{self.class} is a Singleton class." if self.class.count > 1
+  end
 end
