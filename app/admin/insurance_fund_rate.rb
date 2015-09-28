d4f09e454ac49e94b5cac60dcf644d3ea6bc64c7
wendi <ifyouseewendy@gmail.com>
2015-09-28 14:24:19 +0800

    insurance_fund_rate: Create AA resource

---
+++ app/admin/insurance_fund_rate.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register InsuranceFundRate do
+  actions :all, except: [:new, :create, :destroy]
+
+  menu \
+    parent: I18n.t("activerecord.models.settings"),
+    priority: 2
+
+  config.batch_actions = false
+  config.clear_action_items!
+  config.filters = false
+
+
+  index download_links: false do
+    InsuranceFundRate.ordered_columns(without_base_keys: true, without_foreign_keys: true).map do |field|
+      column field
+    end
+    actions defaults: false do |ifr|
+      item "编辑", edit_insurance_fund_rate_path(ifr)
+    end
+  end
+
+  sidebar "说明" do
+    ol do
+      li span "五险一金缴费比例会用于计算工资表中的五险一金，如有问题请及时反馈。"
+      li span "各比例均可通过'编辑' 按钮进行更新。"
+    end
+  end
+end
--- app/models/insurance_fund_rate.rb
+++ app/models/insurance_fund_rate.rb
@@ -1,8 +1,19 @@
 class InsuranceFundRate < ActiveRecord::Base
   include ActAsSingleton
 
-  # The model contains only 2 records, maybe called Dounbleton
+  # The model contains only 2 records, maybe called Doubleton
   def confirm_singularity
     raise "#{self.class} is a Singleton class." if self.class.count > 1
+  end
+
+  class << self
+    def ordered_columns(without_base_keys: false, without_foreign_keys: false)
+      names = column_names.map(&:to_sym)
+
+      names -= %i(id created_at updated_at) if without_base_keys
+      names -= %i() if without_foreign_keys
+
+      names
+    end
   end
 end
