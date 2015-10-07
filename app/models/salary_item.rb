7503bb8f88477f49ec2400690dfe3c6cacb65911
wendi <ifyouseewendy@gmail.com>
2015-10-07 19:01:33 +0800

    salary_item: Fix batch_form_fields

--- app/models/salary_item.rb
+++ app/models/salary_item.rb
@@ -43,7 +43,9 @@ class SalaryItem < ActiveRecord::Base
     end
 
     def batch_form_fields
-      column_types.map.each_with_object({}){|(k,v), ha| ha[ "#{k}_#{SalaryItem.human_attribute_name(k)}" ] = :text }
+      fields = ordered_columns(without_base_keys: true, without_foreign_keys: true)\
+        - [:total_personal, :salary_in_fact, :total_company, :total_sum, :total_sum_with_admin_amount]
+      fields.each_with_object({}){|k, ha| ha[ "#{k}_#{SalaryItem.human_attribute_name(k)}" ] = :text }
     end
   end
 
