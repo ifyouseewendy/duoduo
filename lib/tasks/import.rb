6679fd5bb97325fdec350c7b2e408975ffcce3c4
wendi <ifyouseewendy@gmail.com>
2015-10-13 23:13:34 +0800

    import: Add parsing sheet 1

--- lib/tasks/import.rb
+++ lib/tasks/import.rb
@@ -29,9 +29,9 @@ class Import < Thor
 
     failed = [ [], [], [], [] ]
 
-    puts "--> Start processing Sheet 0"
     sheet_id = 0
     sheet = xlsx.sheet(sheet_id)
+    puts "--> Start processing Sheet #{sheet_id}"
 
     # 档案编号, 姓名, 单位编号, 公司名称, 身份证号, 年龄, 性别, 民族, 学历, 家庭住址, 联系电话, 社保参加工作时间, 本单位社保参保时间, 本单位医保参保时间, 到本单位时间, 四平东方合同起止时间, 通讯合同起止时间, 社保个人编号, 医保个人编号, 医保卡号, 备案时间, 备案地, 工作地点, 工种, 备注, 公主岭合同起止时间, 公主岭社保个人编号, 公主岭医保个人编号
 
@@ -81,6 +81,8 @@ class Import < Thor
         #   :medical_insurance_base
         #   :house_accumulation_base
         #   :release_date
+        #   :social_insurance_release_date
+        #   :medical_insurance_release_date
         contract_attrs = {
           contract_type: contract_type,
           in_contract: true,
@@ -140,6 +142,119 @@ class Import < Thor
       end
     end
 
+    sheet_id = 1
+    sheet = xlsx.sheet(sheet_id)
+    puts "--> Start processing Sheet #{sheet_id}"
+
+
+    # 档案编号, 姓名, 单位编号, 公司名称, 身份证号, 年龄, 性别, 民族, 学历, 家庭住址, 联系电话, 社保参加工作时间, 本单位社保参保时间, 本单位医保参保时间, 到本单位时间, 四平东方合同起止时间, 通讯合同起止时间 , 社保个人编号, 医保个人编号, 医保卡号, 备案时间, 备案地, 工作地点, 工种, 备注, 公主岭合同起止时间, 公主岭社保个人编号, 公主岭医保个人编号, 办理解除时间, 社保解除时间, 医保解除时间
+
+    last_row = sheet.last_row
+    (2..last_row).each do |row_id|
+      puts "... Processing #{row_id}/#{last_row}" if row_id % 100 == 0
+
+      nest_index, name, corporation_id, corporation_name, identity_card, age, gender, nation, grade, address, telephone, social_insurance_start_date, current_social_insurance_start_date, current_medical_insurance_start_date, arrive_current_company_at, dongfang_siping_contract_dates, current_contract_dates, social_insurance_serial, medical_insurance_serial, medical_insurance_card, backup_date, backup_place, work_place, work_type, remark, dongfang_gongzhuling_contract_dates, dongfang_gongzhuling_social_insurance_serial, dongfang_gongzhuling_medical_insurance_serial, release_date, social_insurance_release_date, medical_insurance_release_date = \
+        sheet.row(row_id).map{|col| String === col ? col.strip : col}
+
+      in_service = false
+      contract_type = :normal_contract
+
+      begin
+        normal_corporation_id = NormalCorporation.find_or_create_by!(name: corporation_name).id
+        raise "未找到合作单位(名称: #{corporation_name})" if normal_corporation_id.nil?
+
+        # Need to confirm
+        #
+        #   :account
+        #   :account_bank
+        #   :birth
+        ns = NormalStaff.create!(
+          nest_index: nest_index.to_i,
+          name: name,
+          account: nil,
+          account_bank: nil,
+          identity_card: identity_card.to_s,
+          birth: nil,
+          age: age,
+          gender: {'男' => 'male', '女' => 'female'}[gender],
+          nation: nation,
+          grade: grade,
+          address: address,
+          telephone: telephone.to_s,
+          social_insurance_start_date: parse_date(social_insurance_start_date),
+          in_service: in_service,
+          remark: remark,
+          normal_corporation_id: normal_corporation_id,
+          sub_company_id: sub_company_id
+        )
+
+        # Need to confirm
+        #
+        #   :has_accident_insurance
+        #   :social_insurance_base
+        #   :medical_insurance_base
+        #   :house_accumulation_base
+        #   :release_date
+        contract_attrs = {
+          contract_type: contract_type,
+          in_contract: false,
+          contract_start_date: parse_date(current_contract_dates.split('-')[0]),
+          contract_end_date: parse_date(current_contract_dates.split('-')[1]),
+          arrive_current_company_at: parse_date(arrive_current_company_at),
+          has_social_insurance: true,
+          has_medical_insurance: true,
+          has_accident_insurance: nil,
+          current_social_insurance_start_date: parse_date(current_social_insurance_start_date),
+          current_medical_insurance_start_date: parse_date(current_medical_insurance_start_date),
+          social_insurance_base: nil.to_i,
+          medical_insurance_base: nil.to_i,
+          house_accumulation_base: nil.to_i,
+          social_insurance_serial: social_insurance_serial.to_s,
+          medical_insurance_serial: medical_insurance_serial.to_s,
+          medical_insurance_card: medical_insurance_card.to_s,
+          backup_date: parse_date(backup_date),
+          backup_place: backup_place,
+          work_place: work_place,
+          work_type: work_type,
+          release_date: release_date,
+          social_insurance_release_date: parse_date(social_insurance_release_date),
+          medical_insurance_release_date: parse_date(medical_insurance_release_date),
+          remark: nil,
+          sub_company_id: sub_company_id,
+          normal_corporation_id: normal_corporation_id,
+          normal_staff_id: ns.id,
+        }
+
+        # Create current contract
+        LaborContract.create!(contract_attrs)
+
+        # Create dongfang siping contract
+        if dongfang_siping_contract_dates.present?
+          contract_attrs_siping = contract_attrs.merge({
+            in_contract: false,
+            contract_start_date: parse_date(dongfang_siping_contract_dates.split('-')[0]),
+            contract_end_date: parse_date(dongfang_siping_contract_dates.split('-')[1]),
+          })
+          LaborContract.create!(contract_attrs_siping)
+        end
+
+        # Create dongfang gongzhuling contract
+        if dongfang_gongzhuling_contract_dates.present?
+          contract_attrs_gongzhuling = contract_attrs.merge({
+            in_contract: false,
+            contract_start_date: parse_date(dongfang_gongzhuling_contract_dates.split('-')[0]),
+            contract_end_date: parse_date(dongfang_gongzhuling_contract_dates.split('-')[1]),
+            social_insurance_serial: dongfang_gongzhuling_social_insurance_serial,
+            medical_insurance_serial: dongfang_gongzhuling_medical_insurance_serial
+          })
+          LaborContract.create!(contract_attrs_gongzhuling)
+        end
+      rescue => e
+        failed[sheet_id] << sheet.row(row_id) + [e.message, e.backtrace]
+      end
+
+    end
+
     if failed.any?(&:present?)
       filename = "#{file.basename.to_s.split('.')[0]}.#{Time.stamp}.xlsx"
       filepath = Pathname("tmp/#{filename}")
@@ -155,7 +270,7 @@ class Import < Thor
       end
 
       puts "--> Generate failed file: #{filepath.expand_path}"
-      `open #{fielpath.expand_path}`
+      `open #{filepath.expand_path}`
     else
       puts "--> Bravo, Succeed!"
     end
