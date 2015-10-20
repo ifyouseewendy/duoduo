4c9ee8c30ddeb7b029e160e20719c7bb9aeb4df9
wendi <ifyouseewendy@gmail.com>
2015-10-20 13:29:04 +0800

    helper: Fix for guard salary table

--- app/helpers/active_admin/salary_items_helper.rb
+++ app/helpers/active_admin/salary_items_helper.rb
@@ -1,9 +1,13 @@
 module ActiveAdmin::SalaryItemsHelper
   def salary_table
-    @_salary_table ||= SalaryTable.find(params[:salary_table_id])
+    @_salary_table ||= SalaryTable.where(id: params[:salary_table_id]).first
+  end
+
+  def guard_salary_table
+    @_guard_salary_table ||= GuardSalaryTable.where(id: params[:guard_salary_table_id]).first
   end
 
   def normal_corporation
-    @_normal_corporation ||= salary_table.normal_corporation
+    @_normal_corporation ||= [salary_table, guard_salary_table].detect{|st| st.try(:normal_corporation)}
   end
 end
