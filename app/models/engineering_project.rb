5feb4130401558d5ebc792d16488908f0e0d9f4f
wendi <ifyouseewendy@gmail.com>
2015-10-21 16:03:24 +0800

    engi project: Add after save callback to revise fields

--- app/models/engineering_project.rb
+++ app/models/engineering_project.rb
@@ -1,3 +1,16 @@
 class EngineeringProject < ActiveRecord::Base
   belongs_to :engineering_customer
+
+  before_save :revise_fields
+
+  def revise_fields
+    self.project_range ||= -> {
+      month = (project_end_date.to_date - project_start_date.to_date).to_i / 29
+      "#{month} 月"
+    }.call
+
+    if (changed && [:project_amount, :admin_amount]).present?
+      self.total_amount = project_amount + admin_amount
+    end
+  end
 end
