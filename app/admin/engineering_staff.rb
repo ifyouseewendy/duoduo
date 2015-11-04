25a72dc71bbf89db42e836fd920c1fd60e0e6a36
wendi <ifyouseewendy@gmail.com>
2015-11-04 22:37:21 +0800

    engi staff: Update index, show, and form

--- app/admin/engineering_staff.rb
+++ app/admin/engineering_staff.rb
@@ -1,15 +1,27 @@
 ActiveAdmin.register EngineeringStaff do
+  belongs_to :engineering_customer, optional: true
   include ImportSupport
 
   menu \
-    parent: I18n.t("activerecord.models.staff"),
-    priority: 22
+    parent: I18n.t("activerecord.models.engineering_business"),
+    priority: 4
 
   permit_params *EngineeringStaff.ordered_columns(without_base_keys: true, without_foreign_keys: true)
 
   index do
     selectable_column
-    EngineeringStaff.ordered_columns.map(&:to_sym).map do |field|
+
+    column :id
+    column :nest_index
+    column :name
+    column :engineering_customer, sortable: :id do |obj|
+      link_to obj.engineering_customer.name, engineering_customer_path(obj.engineering_customer)
+    end
+    column :engineering_projects, sortable: :id do |obj|
+      link_to "所属项目", "/engineering_projects?utf8=✓&q%5Bengineering_staffs_id_eq%5D=#{obj.id}&commit=过滤&order=id_desc"
+    end
+
+    (EngineeringStaff.ordered_columns(without_foreign_keys: true) - [:id, :nest_index, :name]).map(&:to_sym).map do |field|
       if field == :gender
         # enum
         column :gender do |obj|
@@ -22,11 +34,15 @@ ActiveAdmin.register EngineeringStaff do
     actions
   end
 
+  preserve_default_filters!
+  remove_filter :salary_items
+
   form do |f|
     f.semantic_errors *f.object.errors.keys
 
     f.inputs do
-      f.input :engineering_staffs_corporation, as: :select
+      f.input :engineering_customer, collection: EngineeringCustomer.all
+      f.input :engineering_projects, as: :select, collection: EngineeringProject.all
       f.input :nest_index, as: :number
       f.input :name, as: :string
       f.input :identity_card, as: :string
@@ -41,10 +57,40 @@ ActiveAdmin.register EngineeringStaff do
     f.actions
   end
 
+  member_action :update, method: :post do
+    attrs = params.require(:engineering_staff).permit( EngineeringStaff.ordered_columns + [engineering_project_ids: []] )
+
+    begin
+      obj = EngineeringStaff.find(params[:id])
+      obj.update! attrs
+
+      redirect_to engineering_staff_path(obj), notice: "成功更新工程员工信息"
+    rescue => e
+      if e.message == "Staff refuse the project schedule"
+        alert = "更新失败，多个工程项目的起止日期有重叠。"
+        projects = params[:engineering_staff][:engineering_project_ids].map{|id| EngineeringProject.where(id: id).first }.reject(&:blank?)
+        alert += projects.map{|pr| pr.range_output}.join('；')
+      else
+        alert = e.message
+      end
+      redirect_to engineering_staff_path(obj), alert: alert
+    end
+  end
+
   show do
     attributes_table do
+      row :id
+      row :nest_index
+      row :name
+      row :engineering_customer do |obj|
+        link_to obj.engineering_customer.name, engineering_customer_path(obj.engineering_customer)
+      end
+      row :engineering_projects do |obj|
+        link_to "所属项目", "/engineering_projects?utf8=✓&q%5Bengineering_staffs_id_eq%5D=#{obj.id}&commit=过滤&order=id_desc"
+      end
+
       boolean_columns = EngineeringStaff.columns_of(:boolean)
-      EngineeringStaff.ordered_columns.map(&:to_sym).map do |field|
+      (EngineeringStaff.ordered_columns(without_foreign_keys: true) - [:id, :nest_index, :name]).map(&:to_sym).map do |field|
         if boolean_columns.include? field
           row(field) { status_tag resource.send(field).to_s }
         else
--- app/models/engineering_staff.rb
+++ app/models/engineering_staff.rb
@@ -10,7 +10,7 @@ class EngineeringStaff < ActiveRecord::Base
       names = column_names.map(&:to_sym)
 
       names -= %i(id created_at updated_at) if without_base_keys
-      names -= %i() if without_foreign_keys
+      names -= %i(engineering_customer_id) if without_foreign_keys
 
       names
     end
