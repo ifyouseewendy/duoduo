0fc94286b9ddd417765aeec59f6af6dc04ca43aa
wendi <ifyouseewendy@gmail.com>
2015-11-05 09:27:37 +0800

    engi customer: Add export support

--- app/models/engineering_customer.rb
+++ app/models/engineering_customer.rb
@@ -18,6 +18,41 @@ class EngineeringCustomer < ActiveRecord::Base
       fields.each{|k| hash[ "#{k}_#{human_attribute_name(k)}" ] = :text }
       hash
     end
+
+    def export_xlsx(options: {})
+      filename = "#{I18n.t("activerecord.models.#{name.underscore}")}_#{Time.stamp}.xlsx"
+      filepath = EXPORT_PATH.join filename
+
+      collection = self.all
+      collection = collection.where(id: options[:selected]) if options[:selected].present?
+
+      columns = columns_based_on(options: options)
+
+      Axlsx::Package.new do |p|
+        p.workbook.add_worksheet(name: name) do |sheet|
+          sheet.add_row columns.map{|col| self.human_attribute_name(col)}
+
+          collection.each do |item|
+             stats = \
+              columns.map do |col|
+                item.send(col)
+              end
+              sheet.add_row stats
+          end
+        end
+        p.serialize(filepath.to_s)
+      end
+
+      filepath
+    end
+
+    def columns_based_on(options: {})
+      if options[:columns].present?
+        options[:columns].map(&:to_sym)
+      else
+        ordered_columns(without_foreign_keys: true)
+      end
+    end
   end
 
   def free_staffs(start_date, end_date)
