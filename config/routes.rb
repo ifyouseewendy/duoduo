23f6581f354c2e49ffe274215f633138413b0ecb
wendi <ifyouseewendy@gmail.com>
2015-11-08 15:46:10 +0800

    engi st: Update routes mapping subclass

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  resources :engineering_normal_salary_tables, controller: "engineering_salary_tables"
+  resources :engineering_normal_with_tax_salary_tables, controller: "engineering_salary_tables"
+  resources :engineering_big_table_salary_tables, controller: "engineering_salary_tables"
+  resources :engineering_dong_fang_salary_tables, controller: "engineering_salary_tables"
+
   ActiveAdmin.routes(self)
 
   resources :contract_files, only: [:create, :destroy] do
