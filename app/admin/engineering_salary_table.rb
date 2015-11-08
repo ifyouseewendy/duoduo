c150492164ca24eceaefe61d1320a6abc8886ed6
wendi <ifyouseewendy@gmail.com>
2015-11-08 14:39:13 +0800

    engi st: Fully customize index actions

--- app/admin/engineering_salary_table.rb
+++ app/admin/engineering_salary_table.rb
@@ -13,14 +13,19 @@ ActiveAdmin.register EngineeringSalaryTable do
     column :type, sortable: :id do |obj|
       obj.model_name.human
     end
-    column :engineering_project, sortable: :id do |obj|
+    column :engineering_project, sortable: :engineering_project_id do |obj|
       link_to obj.engineering_project.name, engineering_project_path(obj.engineering_project)
     end
     column :remark
     column :created_at
     column :updated_at
 
-    actions do
+    actions defaults: false do |obj|
+      item "查看", engineering_salary_table_path(obj)
+      text_node "&nbsp;".html_safe
+      item "编辑", edit_engineering_salary_table_path(obj)
+      text_node "&nbsp;".html_safe
+      item "删除", engineering_salary_table_path(obj), method: :delete
       text_node "&nbsp;|&nbsp;&nbsp;".html_safe
       item "工资表", "#"
     end
