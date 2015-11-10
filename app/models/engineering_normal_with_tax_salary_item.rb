54f3eade3a7b1b802a8c519da146c417da490e1a
wendi <ifyouseewendy@gmail.com>
2015-11-10 21:21:48 +0800

    engi si: Add normal tax item create_by and revise_fields

--- app/models/engineering_normal_with_tax_salary_item.rb
+++ app/models/engineering_normal_with_tax_salary_item.rb
@@ -5,4 +5,27 @@ class EngineeringNormalWithTaxSalaryItem < ActiveRecord::Base
     inverse_of: :salary_items
 
   belongs_to :engineering_staff
+
+  before_save :revise_fields
+
+  class << self
+    def create_by(table:, staff:, salary_deserve:)
+      item = self.new(salary_table: table, engineering_staff: staff)
+
+      item.salary_deserve     = salary_deserve
+      item.social_insurance   = 407
+      item.medical_insurance  = 249
+
+      item.save!
+    end
+  end
+
+  def revise_fields
+    if (changed && [:salary_deserve, :social_insurance, :medical_insurance]).present?
+      self.total_insurance  = self.social_insurance + self.medical_insurance
+      self.total_amount     = self.salary_deserve + self.total_insurance
+      self.tax              = IndividualIncomeTax.calculate(salary: self.total_amount)
+      self.salary_in_fact   = self.total_amount - self.tax
+    end
+  end
 end
