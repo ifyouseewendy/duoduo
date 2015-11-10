9f6d31708256bd314aca4ca30ed1debfb3908e0a
wendi <ifyouseewendy@gmail.com>
2015-11-10 21:28:26 +0800

    engi si: Add normal tax item resource and model

---
+++ app/admin/engineering_normal_with_tax_salary_item.rb
@@ -0,0 +1,95 @@
+ActiveAdmin.register EngineeringNormalWithTaxSalaryItem do
+  menu false
+
+  config.clear_action_items!
+
+  breadcrumb do
+    if params['q'].present?
+      st = EngineeringSalaryTable.find(params['q']['salary_table_id_eq'])
+      [
+        link_to(st.engineering_project.name, engineering_project_path(st.engineering_project) ),
+        link_to(st.name, engineering_salary_table_path(st) )
+      ]
+    else
+      []
+    end
+  end
+
+  index do
+    selectable_column
+
+    column :id
+    column :name, sortable: ->(obj){ obj.engineering_staff.name } do |obj|
+      staff = obj.engineering_staff
+      link_to staff.name, engineering_staff_path(staff)
+    end
+    (EngineeringNormalWithTaxSalaryItem.ordered_columns(without_foreign_keys: true) - [:id]).each do |field|
+      column field
+    end
+
+    actions
+  end
+
+  preserve_default_filters!
+  remove_filter :engineering_staff
+
+  permit_params *EngineeringNormalWithTaxSalaryItem.ordered_columns(without_base_keys: true, without_foreign_keys: false)
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs do
+      f.input :salary_deserve, as: :number
+      f.input :social_insurance, as: :number
+      f.input :medical_insurance, as: :number
+      f.input :remark, as: :text
+    end
+
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name do |obj|
+        staff = obj.engineering_staff
+        link_to staff.name, engineering_staff_path(staff)
+      end
+      row :salary_table do |obj|
+        st = obj.salary_table
+        link_to st.name, engineering_salary_table_path(st)
+      end
+      row :engineering_project do |obj|
+        pr = obj.salary_table.engineering_project
+        link_to pr.name, engineering_project_path(pr)
+      end
+      (EngineeringNormalSalaryItem.ordered_columns(without_foreign_keys: true) - [:id]).each do |field|
+        row field
+      end
+    end
+    active_admin_comments
+  end
+
+  # Batch actions
+  batch_action :batch_edit, form: EngineeringNormalWithTaxSalaryItem.batch_form_fields do |ids|
+    inputs = JSON.parse(params['batch_action_inputs']).with_indifferent_access
+
+    batch_action_collection.find(ids).each do |obj|
+      obj.update(inputs)
+    end
+
+    redirect_to :back, notice: "成功更新 #{ids.count} 条记录"
+  end
+
+  # Collection actions
+  collection_action :export_xlsx do
+    options = {}
+    options[:selected] = params[:selected].split('-') if params[:selected].present?
+    options[:columns] = params[:columns].split('-') if params[:columns].present?
+    options[:salary_table_id] = params[:q][:salary_table_id_eq] rescue nil
+
+    file = EngineeringNormalWithTaxSalaryItem.export_xlsx(options: options)
+    send_file file, filename: file.basename
+  end
+
+end
--- app/models/engineering_normal_with_tax_salary_item.rb
+++ app/models/engineering_normal_with_tax_salary_item.rb
@@ -18,6 +18,70 @@ class EngineeringNormalWithTaxSalaryItem < ActiveRecord::Base
 
       item.save!
     end
+
+    def ordered_columns(without_base_keys: false, without_foreign_keys: false)
+      names = column_names.map(&:to_sym)
+
+      names -= %i(id created_at updated_at) if without_base_keys
+      names -= %i(engineering_salary_table_id engineering_staff_id) if without_foreign_keys
+
+      names
+    end
+
+    def batch_form_fields
+      hash = {}
+      fields = [:salary_deserve, :social_insurance, :medical_insurance]
+      fields.each{|k| hash[ "#{k}_#{human_attribute_name(k)}" ] = :text }
+      hash
+    end
+
+    def export_xlsx(options: {})
+      collection = self.all
+      names = [self.model_name.human]
+
+      if options[:selected].present?
+        collection = collection.where(id: options[:selected])
+      elsif options[:salary_table_id].present?
+        salary_table = EngineeringSalaryTable.find(options[:salary_table_id])
+        collection = salary_table.salary_items
+        names += [salary_table.engineering_project.name, salary_table.name]
+      end
+      names << Time.stamp
+
+      filename = "#{names.join('_')}.xlsx"
+      filepath = EXPORT_PATH.join filename
+
+      columns = columns_based_on(options: options)
+      Axlsx::Package.new do |p|
+        p.workbook.add_worksheet(name: name[0,31]) do |sheet|
+          sheet.add_row columns.map{|col| self.human_attribute_name(col)}
+
+          collection.each do |item|
+             stats = \
+              columns.map do |col|
+                if [:engineering_staff].include? col
+                  item.send(col).name
+                else
+                  item.send(col)
+                end
+              end
+              sheet.add_row stats
+          end
+        end
+        p.serialize(filepath.to_s)
+      end
+
+      filepath
+    end
+
+    def columns_based_on(options: {})
+      if options[:columns].present?
+        options[:columns].map(&:to_sym)
+      else
+        %i(id engineering_staff) \
+          + (ordered_columns(without_foreign_keys: true) - %i(id))
+      end
+    end
   end
 
   def revise_fields
