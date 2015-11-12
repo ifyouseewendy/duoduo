e5337bd3a64f61346374bedba3dc4eff77d6f748
wendi <ifyouseewendy@gmail.com>
2015-11-12 11:33:50 +0800

    engi file: Add proxy file upload

--- app/admin/engineering_project.rb
+++ app/admin/engineering_project.rb
@@ -96,7 +96,10 @@ ActiveAdmin.register EngineeringProject do
     end
 
     panel "工程合同（劳务派遣协议）" do
-      render partial: 'shared/contract_engineering', locals: { contract_files: resource.contract_files, engineering_project_id: resource.id, role: :normal }
+      render partial: 'shared/contract_engineering', locals: { contract_files: resource.contract_files.normal, engineering_project_id: resource.id, role: :normal }
+    end
+    panel "代发协议" do
+      render partial: 'shared/contract_engineering', locals: { contract_files: resource.contract_files.proxy, engineering_project_id: resource.id, role: :proxy }
     end
     active_admin_comments
   end
