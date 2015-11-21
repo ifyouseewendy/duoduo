db2fab0f97709d6b447abcf345aff9eddca8a75f
wendi <ifyouseewendy@gmail.com>
2015-11-21 22:10:47 +0800

    outcome items: Update to validates persons

--- app/models/engineering_outcome_item.rb
+++ app/models/engineering_outcome_item.rb
@@ -1,7 +1,7 @@
 class EngineeringOutcomeItem < ActiveRecord::Base
   belongs_to :project, class: EngineeringProject, foreign_key: :engineering_project_id
 
-  validates_presence_of :date, :amount
+  validates_presence_of :persons
 
   default_scope { order('created_at DESC') }
 end
