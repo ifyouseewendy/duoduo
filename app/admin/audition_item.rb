4acba35976dda3e9d29628996bd25cd6b1651e73
wendi <ifyouseewendy@gmail.com>
2015-11-22 18:00:49 +0800

    audition: Add apply action

--- app/admin/audition_item.rb
+++ app/admin/audition_item.rb
@@ -4,6 +4,8 @@ ActiveAdmin.register AuditionItem do
     priority: 6,
     if: -> {current_admin_user.finance_admin?}
 
+  actions :all, except: [:show, :new, :create, :edit, :update, :destroy]
+
   config.batch_actions = false
   config.clear_action_items!
   config.filters = false
@@ -26,5 +28,27 @@ ActiveAdmin.register AuditionItem do
     column :updated_at
 
     actions defaults: false
+  end
+
+  collection_action :apply do
+    begin
+      ai = AuditionItem.find_or_create_by!(
+        auditable_id: params[:auditable_id],
+        auditable_type: fix_engineering_salary_table(type: params[:auditable_type])
+      )
+
+      ai.update_attribute(:status, :apply_audit)
+      redirect_to :back, notice: "已申请复核"
+    rescue => e
+      redirect_to :back, alert: "操作失败，请把这段信息存入问题反馈：#{Time.now} - #{e.message}"
+    end
+  end
+
+  controller do
+    private
+
+      def fix_engineering_salary_table(type:)
+        EngineeringSalaryTable.types.map(&:to_s).include?(type.strip) ? 'EngineeringSalaryTable' : type
+      end
   end
 end
