25507ef0b8aa5c60461086f2866501f0200172fc
wendi <ifyouseewendy@gmail.com>
2015-12-03 11:37:26 +0800

    duoduo_cli: Add base thor

--- lib/tasks/duoduo_cli.rb
+++ lib/tasks/duoduo_cli.rb
@@ -8,6 +8,16 @@ class DuoduoCli < Thor
     puts "Hello #{name}"
   end
 
+  desc "base", ''
+  def base
+    load_rails
+    clean_db(:business, :engineer, :base)
+
+    seed_admin_user
+    seed_sub_companies
+    seed_insurance_fund
+  end
+
   private
 
     def load_from(from)
@@ -22,19 +32,10 @@ class DuoduoCli < Thor
       require File.expand_path('config/environment.rb')
     end
 
-    def clean_db(types)
-      puts "==> Cleaning DB data"
+    def clean_db(*types)
+      puts "==> Cleaning DB data, strategy: #{types.join(', ')}"
 
-      types = Array.wrap(types)
       collection = []
-
-      if types.include? :base
-        collection += [
-          AdminUser,
-          SubCompany, ContractFile,
-          InsuranceFundRate, IndividualIncomeTaxBase, IndividualIncomeTax,
-        ]
-      end
 
       if types.include? :business
         collection += [
@@ -69,11 +70,27 @@ class DuoduoCli < Thor
         ]
       end
 
-      collection.each(&:destroy_all)
+      if types.include? :base
+        collection += [
+          AdminUser,
+          SubCompany,
+          InsuranceFundRate, IndividualIncomeTaxBase, IndividualIncomeTax,
+        ]
+      end
+
+      collection.uniq.each(&:destroy_all)
     end
 
     def init_logger
       @logger ||= ActiveSupport::Logger.new('log/import.log')
+    end
+
+    def seed_admin_user
+      puts "==> Preparing AdminUser"
+      if AdminUser.where(email: 'admin').first.nil?
+        au = AdminUser.new(email: 'admin', password: '123123', password_confirmation: '123123')
+        au.save(validate: false)
+      end
     end
 
     def seed_sub_companies
@@ -92,6 +109,23 @@ class DuoduoCli < Thor
         companies << sc
       end
     end
+
+    def seed_insurance_fund
+      puts "==> Preparing Individual Income Tax Table"
+      IndividualIncomeTaxBase.create(base: 3500)
+      IndividualIncomeTax.create(grade: 1, tax_range_start: 0,     tax_range_end: 1500,       rate: 0.03)
+      IndividualIncomeTax.create(grade: 2, tax_range_start: 1500,  tax_range_end: 4500,       rate: 0.1)
+      IndividualIncomeTax.create(grade: 3, tax_range_start: 4500,  tax_range_end: 9000,       rate: 0.2)
+      IndividualIncomeTax.create(grade: 4, tax_range_start: 9000,  tax_range_end: 35000,      rate: 0.25)
+      IndividualIncomeTax.create(grade: 5, tax_range_start: 35000, tax_range_end: 55000,      rate: 0.3)
+      IndividualIncomeTax.create(grade: 6, tax_range_start: 55000, tax_range_end: 80000,      rate: 0.35)
+      IndividualIncomeTax.create(grade: 7, tax_range_start: 80000, tax_range_end: 999999999,  rate: 0.45)
+
+      puts "==> Preparing Insurance Fund Rate Table"
+      InsuranceFundRate.create(name: '个人', pension: 0.08, unemployment: 0.005, medical: 0.02, injury: 0, birth: 0, house_accumulation: 96)
+      InsuranceFundRate.create(name: '公司', pension: 0.2,  unemployment: 0.015, medical: 0.06, injury: 0.015, birth: 0.004, house_accumulation: 96)
+
+    end
 end
 
-
+DuoduoCli.start(ARGV)
