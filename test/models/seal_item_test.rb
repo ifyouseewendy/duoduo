c9634c97debc687e55386e3fe92ef277df19c43f
wendi <ifyouseewendy@gmail.com>
2015-12-06 20:58:33 +0800

    seal item: Add test on query_user

--- test/models/seal_item_test.rb
+++ test/models/seal_item_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class SealItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "query_user" do
+    assert_equal 'first table', SealItem.query_user(name: 'wendi')
+    assert_equal 'first table', SealItem.query_user(name: 'larry')
+    assert_equal 'second table', SealItem.query_user(name: 'dapian')
+  end
 end
