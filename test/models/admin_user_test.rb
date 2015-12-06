47478a723170094399c39b8d2755a34849af9c4d
wendi <ifyouseewendy@gmail.com>
2015-12-06 19:47:28 +0800

    admin_user: Add test and fixtures

--- test/models/admin_user_test.rb
+++ test/models/admin_user_test.rb
@@ -1,14 +1,24 @@
 require 'test_helper'
 
 class AdminUserTest < ActiveSupport::TestCase
-  self.use_instantiated_fixtures = true
+  def test_admin
+    assert admin_users(:super_admin).admin?
+    assert admin_users(:finance_admin).admin?
+    assert admin_users(:business_admin).admin?
 
-  test "role" do
-    # assert @alpha.in_administration?
-    #
-    # @alpha.in_business!
-    # assert @alpha.in_business?
-    #
-    # refute @alpha.in_finance?
+    refute admin_users(:finance_senior).admin?
+    refute admin_users(:finance_junior).admin?
+  end
+
+  def test_finance_admin
+    assert admin_users(:super_admin).finance_admin?
+    assert admin_users(:finance_admin).finance_admin?
+    refute admin_users(:finance_senior).finance_admin?
+  end
+
+  def test_finance_normal
+    refute admin_users(:finance_admin).finance_normal?
+    assert admin_users(:finance_senior).finance_normal?
+    assert admin_users(:finance_junior).finance_normal?
   end
 end
