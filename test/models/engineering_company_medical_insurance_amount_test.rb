9d48540bab188e491ca6685d57197bde8b3f2d33
wendi <ifyouseewendy@gmail.com>
2015-12-06 22:46:15 +0800

    engi_medical_insruance: Add test

--- test/models/engineering_company_medical_insurance_amount_test.rb
+++ test/models/engineering_company_medical_insurance_amount_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class EngineeringCompanyMedicalInsuranceAmountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "current" do
+    assert_equal 3, EngineeringCompanyMedicalInsuranceAmount.current.amount
+  end
+
+  test "query_amount" do
+    assert_equal 2, EngineeringCompanyMedicalInsuranceAmount.query_amount(date: '2015-02-15')
+  end
 end
