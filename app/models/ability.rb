c18c0cfda58038de3585463c482da02377b8bdd9
wendi <ifyouseewendy@gmail.com>
2015-12-06 19:13:12 +0800

    cancan: Remove
    This reverts commit 2a83aa805aa2910e8b268241bfac729d25cac5b5.
    
    Conflicts:
    	app/controllers/application_controller.rb
    	config/initializers/active_admin.rb

--- app/models/ability.rb
+++
@@ -1,9 +0,0 @@
-class Ability
-  include CanCan::Ability
-
-  def initialize(user)
-    can :manage, Project
-    can :read, Project
-    can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: :admin
-  end
-end
