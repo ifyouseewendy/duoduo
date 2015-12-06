c0253ae61c3c83393ecdbd3f9966e9227c94302e
wendi <ifyouseewendy@gmail.com>
2015-12-06 22:45:32 +0800

    engi_si_with_normal: Test on revise_fields

--- test/models/engineering_normal_with_tax_salary_item_test.rb
+++ test/models/engineering_normal_with_tax_salary_item_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class EngineeringNormalWithTaxSalaryItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "revise_fields" do
+    item = engineering_normal_with_tax_salary_items(:one)
+
+    item.social_insurance += 10
+
+    assert_difference ['item.total_insurance', 'item.total_amount'], 10 do
+      item.save!
+    end
+  end
 end
