88977228c77945cbae083cba75be875a5c797086
wendi <ifyouseewendy@gmail.com>
2015-12-07 15:25:20 +0800

    public activity: Disable in test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'public_activity/testing'
+
+PublicActivity.enabled = false
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
