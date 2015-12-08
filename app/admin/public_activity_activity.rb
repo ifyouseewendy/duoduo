6c3bd9e893e9db0335fa05980f0b2e51160a0508
wendi <ifyouseewendy@gmail.com>
2015-12-08 11:39:05 +0800

    public_activity: Config AA resource

--- app/admin/public_activity_activity.rb
+++ app/admin/public_activity_activity.rb
@@ -1,5 +1,13 @@
 ActiveAdmin.register PublicActivity::Activity do
-  index as: :block do |activity|
+  menu \
+    parent: I18n.t("activerecord.models.settings"),
+    priority: 6
+
+  config.batch_actions = false
+  config.clear_action_items!
+  config.filters = false
+
+  index as: :block, download_links: false do |activity|
     render partial: 'public_activity_activities/display', locals: {activity: activity}
   end
 
