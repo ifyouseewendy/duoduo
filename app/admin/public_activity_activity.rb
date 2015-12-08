d771a1a207a543fdb6d83feecb690141c0b02719
wendi <ifyouseewendy@gmail.com>
2015-12-08 12:36:55 +0800

    public_activity: Add filter

--- app/admin/public_activity_activity.rb
+++ app/admin/public_activity_activity.rb
@@ -5,10 +5,19 @@ ActiveAdmin.register PublicActivity::Activity do
 
   config.batch_actions = false
   config.clear_action_items!
-  config.filters = false
 
   index as: :block, download_links: false do |activity|
     render partial: 'public_activity_activities/display', locals: {activity: activity}
   end
 
+  filter :owner_id, as: :select, collection: ->{ AdminUser.pluck(:name, :id) }
+  filter :key_cont, as: :select, collection: [['创建', 'create'], ['更新', 'update'], ['删除', 'destroy']]
+  filter :trackable_type
+  filter :created_at
+  # preserve_default_filters!
+  # remove_filter :trackable_type
+  # remove_filter :owner_type
+  # remove_filter :key
+  # remove_filter :parameters
+  # remove_filter :updated_at
 end
