9f2546d886fa590c93df1b4924bb9a351975474d
wendi <ifyouseewendy@gmail.com>
2015-12-16 16:20:11 +0800

    business_corp: Add thor to import corporation info

---
+++ lib/tasks/business_corporation.rb
@@ -0,0 +1,93 @@
+require_relative 'duoduo_cli'
+
+class BusinessCorporation < DuoduoCli
+  attr_reader :file, :xlsx
+
+  desc "test", ''
+  def test
+    load_rails
+    init_logger
+
+    logger.info "Test from Business thor"
+  end
+
+  desc "start", ''
+  long_desc <<-LONGDESC
+    Examples:
+
+      ruby lib/tasks/business_corporation.rb start --from=
+  LONGDESC
+  option :from, required: true
+  def start
+    load_rails
+    clean_db(:business)
+    seed_sub_companies
+    init_logger
+
+    logger.info "[#{Time.now}] Import start"
+
+    set_file load_from(options[:from])
+    parse_file
+
+    logger.info "[#{Time.now}] Import end"
+  end
+
+  private
+
+    def set_file(path)
+      @file = path
+    end
+
+    def set_xlsx
+      @xlsx = Roo::Spreadsheet.open(file.to_s)
+    end
+
+    def parse_file
+      set_xlsx
+
+      xlsx.sheet(0).to_a.each_with_index do |row, idx|
+        next if idx == 0 or idx == 1
+
+        # 单位名称  营业执照号  纳税人识别号  组织代码证号  法人  单位地址  单位账号  单位开户行  单位联系人 单位联系电话  合同签订期限  合同金额  管理费收取方式  管理费收取比例/金额 单位开支日期  备注  吉易子公司
+        name, license, taxpayer_serial, organization_serial, corporate_name, address, account, account_bank, contact, telephone, contract_dates, contract_amount, admin_charge_type, admin_charge_amount, expense_date, remark, sub_company_name = row.map{|col| String === col ? col.strip : col}
+
+        contract_start_date, contract_end_date = *(parse_dates contract_dates)
+        sub_company = SubCompany.find_by_name(sub_company_name)
+
+        nc = NormalCorporation.create!(
+          name: name,
+          license: license,
+          taxpayer_serial: taxpayer_serial,
+          organization_serial: organization_serial,
+          corporate_name: corporate_name,
+          address: address,
+          account: account,
+          account_bank: account_bank,
+          contact: contact,
+          telephone: telephone.to_i.to_s,
+          contract_start_date: contract_start_date,
+          contract_end_date: contract_end_date,
+          contract_amount: contract_amount,
+          admin_charge_type: admin_charge_type,
+          admin_charge_amount: admin_charge_amount,
+          expense_date: parse_chinese_date(expense_date),
+          remark: remark,
+        )
+        nc.sub_companies << sub_company
+      end
+    end
+
+    # 2015年02月28日至2016年02月27日
+    def parse_dates(str)
+      return if str.blank?
+
+      parts = str.split('至')
+      parts.map{|part| parse_chinese_date(part)}
+    end
+
+    def parse_chinese_date(str)
+      Date.parse str.split(/年|月|日/).join('.') rescue nil
+    end
+end
+
+BusinessCorporation.start(ARGV)
