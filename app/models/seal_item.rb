a99fffbb32b09e607a2340ef8139a73dedd85d20
wendi <ifyouseewendy@gmail.com>
2016-01-06 11:35:42 +0800

    seal_item: Auto increment nest_index

--- app/models/seal_item.rb
+++ app/models/seal_item.rb
@@ -3,6 +3,8 @@ class SealItem < ActiveRecord::Base
 
   validates_uniqueness_of :nest_index, scope: :seal_table
 
+  before_save :revise_fields
+
   class << self
     def ordered_columns(without_base_keys: false, without_foreign_keys: false)
       names = column_names.map(&:to_sym)
@@ -16,7 +18,9 @@ class SealItem < ActiveRecord::Base
     def query_user(name:)
       where(name: name.strip).first.try(:seal_table).try(:name)
     end
-
   end
 
+  def revise_fields
+    self.nest_index ||= seal_table.try(:latest_item_index)
+  end
 end
