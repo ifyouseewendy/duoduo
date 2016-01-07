e46b7fb76b168f279be504a47417ddfe50a07695
wendi <ifyouseewendy@gmail.com>
2016-01-07 13:22:55 +0800

    engi_st: Update amount field precision

--- db/migrate/20160106131219_add_amount_to_engineering_salary_tables.rb
+++ db/migrate/20160106131219_add_amount_to_engineering_salary_tables.rb
@@ -1,5 +1,5 @@
 class AddAmountToEngineeringSalaryTables < ActiveRecord::Migration
   def change
-    add_column :engineering_salary_tables, :amount, :decimal, precision: 8, scale: 2
+    add_column :engineering_salary_tables, :amount, :decimal, precision: 12, scale: 2
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -312,12 +312,12 @@ ActiveRecord::Schema.define(version: 20160106131219) do
     t.text     "name"
     t.text     "type"
     t.text     "remark"
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
     t.integer  "engineering_project_id"
     t.date     "start_date"
     t.date     "end_date"
-    t.decimal  "amount",                 precision: 8, scale: 2
+    t.decimal  "amount",                 precision: 12, scale: 2
   end
 
   add_index "engineering_salary_tables", ["engineering_project_id"], name: "index_engineering_salary_tables_on_engineering_project_id", using: :btree
