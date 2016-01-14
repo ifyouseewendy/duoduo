8f841a59490e08bab530515b50166337184466e1
wendi <ifyouseewendy@gmail.com>
2016-01-14 11:21:27 +0800

    engi_staff: Fix batch_edit

--- app/models/engineering_staff.rb
+++ app/models/engineering_staff.rb
@@ -60,13 +60,20 @@ class EngineeringStaff < ActiveRecord::Base
       self.columns_hash.select{|k,v| v.type == type }.keys.map(&:to_sym)
     end
 
+    def batch_fields
+      [
+        :remark
+      ]
+    end
+
     def batch_form_fields
-      fields = ordered_columns(without_base_keys: true, without_foreign_keys: true)
+      fields = batch_fields
       hash = {
-        'engineering_customer_id_工程客户' => EngineeringCustomer.select(:id, :name).reduce([]){|ar, ele| ar << [ele.name, ele.id]},
+        'engineering_customer_id_所属客户' => EngineeringCustomer.as_option(available_project: false),
+        'enable_状态' => [ ['可用', true], ['不可用', false] ],
+        'gender_性别' => [ ['男', 'male'], ['女', 'female'] ],
       }
       fields.each{|k| hash[ "#{k}_#{human_attribute_name(k)}" ] = :text }
-      hash['gender_性别'] = genders_option
       hash
     end
 
