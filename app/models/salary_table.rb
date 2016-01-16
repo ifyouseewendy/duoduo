b4eb44ae4545d5860be799c4d52c63c9348dbe89
wendi <ifyouseewendy@gmail.com>
2016-01-16 14:58:16 +0800

    st: Fix export_xlsx

--- app/models/salary_table.rb
+++ app/models/salary_table.rb
@@ -38,18 +38,64 @@ class SalaryTable < ActiveRecord::Base
     filename = filename_by(view: view)
     filepath = EXPORT_PATH.join filename
 
-    collection = salary_items
+    collection = salary_items.order(nest_index: :asc)
     collection = collection.where(id: options[:selected]) if options[:selected].present?
 
-    columns = SalaryItem.columns_based_on(view: view, options: options)
+    columns = present_fields(view: view, custom: options[:custom])
 
     Axlsx::Package.new do |p|
-      p.workbook.add_worksheet(name: name) do |sheet|
-        sheet.add_row columns.map{|col| SalaryItem.human_attribute_name(col)}
+      wb = p.workbook
+      wrap_text = wb.styles.add_style({
+        alignment: {horizontal: :center, vertical: :center, wrap_text: true},
+        border: {style: :thin, color: '00'}
+      })
 
+      wb.add_worksheet(name: name) do |sheet|
+        # Headers
+        sheet.add_row [ corporation.full_name || corporation.name ], \
+          height: 60, b:true, sz: 16, style: wrap_text
+        sheet.add_row [ start_date.to_s ], \
+          height: 30, b:true, style: wrap_text
+        sheet.add_row columns.map{|col| SalaryItem.human_attribute_name(col)}, \
+          height: 60, b:true, style: wrap_text
+
+        end_col = ('A'.ord + columns.count - 1).chr
+        sheet.merge_cells("A1:#{end_col}1")
+        sheet.merge_cells("A2:#{end_col}2")
+
+        # Content
         collection.each do |item|
-          sheet.add_row columns.map{|col| item.send(col)}
+          data = columns.map do |col|
+            if :staff_account == col
+              "'#{item.send(col).to_s}"
+            else
+              item.send(col).to_s
+            end
+          end
+          sheet.add_row data, style: wrap_text
         end
+
+        # Sum row
+        stats = columns.reduce([]) do |ar, col|
+          if SalaryItem.sum_fields.include?(col)
+            ar << collection.sum(col).to_f.round(2)
+          else
+            ar << nil
+          end
+        end
+        stats[0] = '合计'
+        sheet.add_row stats, style: wrap_text
+
+        end_rol = 3 + collection.count + 1
+        sheet.merge_cells("A#{end_rol}:C#{end_rol}")
+
+        # Footer
+        sheet.add_row []
+        sheet.add_row [ ['经理：', '财务：', '部门经理:', '审核：', '复核：', '制表：'].join('                    ') ]
+
+        # Set First column width
+        # widths = [3, 20, 8] + Array.new(columns.count+1-3){8}
+        sheet.column_widths 5
       end
       p.serialize(filepath.to_s)
     end
@@ -79,6 +125,19 @@ class SalaryTable < ActiveRecord::Base
 
   def available_nest_index
     ( salary_items.order(nest_index: :desc).first.try(:nest_index) || 0 ) + 1
+  end
+
+  def present_fields(view: , custom: )
+    fields = SalaryItem.columns_based_on(view: view, custom: custom)
+    fields.select do |key|
+      salary_items.map{|obj| obj.send(key)}.any? do |val|
+        if Numeric === val
+          val.nonzero?
+        else
+          val.present?
+        end
+      end
+    end
   end
 
 end
