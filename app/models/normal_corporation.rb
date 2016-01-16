78b88d772ee9d6b2ec59759e9f78da3cf2304325
wendi <ifyouseewendy@gmail.com>
2016-01-16 14:10:18 +0800

    normal_corp: Add find_staff

--- app/models/normal_corporation.rb
+++ app/models/normal_corporation.rb
@@ -145,4 +145,19 @@ class NormalCorporation < ActiveRecord::Base
     end
   end
 
+  def find_staff(name:, identity_card: nil)
+    if identity_card.present?
+      staff = normal_staffs.where(identity_card: identity_card).first
+      raise "没有找到员工，身份证号：#{identity_card}" if staff.nil?
+      raise "员工姓名与身份证号不相符，姓名：#{name}，身份证号：#{identity_card}" if name.present? && name != staff.name
+    else
+      raise "找到多个员工，重复姓名：#{name}，请附加一列身份证号" if normal_staffs.where(name: name).count > 1
+
+      staff = normal_staffs.where(name: name).first
+      raise "没有找到员工，姓名：#{name}" if staff.nil?
+    end
+
+    staff
+  end
+
 end
