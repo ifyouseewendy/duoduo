4935563d8b0a96089174c1dc7faf9d227bef411b
wendi <ifyouseewendy@gmail.com>
2016-01-17 11:58:29 +0800

    deploy: Fix mina whenever

--- config/deploy.rb
+++ config/deploy.rb
@@ -115,26 +115,3 @@ namespace :unicorn do
   end
 end
 
-namespace :whenever do
-  desc "Clear crontab"
-  task :clear do
-    queue %{
-      echo "-----> Clear crontab for #{domain}_#{rails_env}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --clear-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
-    }
-  end
-  desc "Update crontab"
-  task :update do
-    queue %{
-      echo "-----> Update crontab for #{domain}_#{rails_env}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --update-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
-    }
-  end
-  desc "Write crontab"
-  task :write do
-    queue %{
-      echo "-----> Update crontab for #{domain}_#{rails_env}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --write-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
-    }
-  end
-end
