3082826f0b9ffc8052d14da33223752b8f6ab112
wendi <ifyouseewendy@gmail.com>
2016-01-27 23:11:18 +0800

    puma: Add prune handler and rm on_work_boot

--- config/puma.rb
+++ config/puma.rb
@@ -8,10 +8,15 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
+prune_bundler
+
+# on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+  #
+  # https://devcenter.heroku.com/articles/concurrency-and-database-connections#threaded-servers
+  # https://github.com/puma/puma/issues/598
+  # ActiveRecord::Base.establish_connection
+# end
 
 stdout_redirect 'log/puma.stdout.log', 'log/puma.stderr.log', true
