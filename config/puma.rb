4b2ef194873a1b47cb2d3c141ff888ba98138481
wendi <ifyouseewendy@gmail.com>
2016-01-28 00:20:51 +0800

    puma: Format config

--- config/puma.rb
+++ config/puma.rb
@@ -2,21 +2,8 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 4)
 threads_count = Integer(ENV['MAX_THREADS'] || 1) # Not sure for thread safe
 threads threads_count, threads_count
 
-# preload_app!
-
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+stdout_redirect 'log/puma.stdout.log', 'log/puma.stderr.log', true
 
 prune_bundler
 
-# on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  #
-  # https://devcenter.heroku.com/articles/concurrency-and-database-connections#threaded-servers
-  # https://github.com/puma/puma/issues/598
-  # ActiveRecord::Base.establish_connection
-# end
-
-stdout_redirect 'log/puma.stdout.log', 'log/puma.stderr.log', true
+# directory "/home/deploy/apps/duoduo/current"
