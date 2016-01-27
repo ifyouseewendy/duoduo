6a6388b285d853e18b0a9b27db7985d94687c8e0
wendi <ifyouseewendy@gmail.com>
2016-01-27 22:52:44 +0800

    puma: Rm preload_app

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 3)
+workers Integer(ENV['WEB_CONCURRENCY'] || 4)
 threads_count = Integer(ENV['MAX_THREADS'] || 1) # Not sure for thread safe
 threads threads_count, threads_count
 
-preload_app!
+# preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
