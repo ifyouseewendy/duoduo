b15722eb523fd7876f37167b4b42a254fbe06941
wendi <ifyouseewendy@gmail.com>
2016-01-27 10:37:10 +0800

    engi_si: Fix export second row

--- app/models/engineering_normal_salary_item.rb
+++ app/models/engineering_normal_salary_item.rb
@@ -150,7 +150,7 @@ class EngineeringNormalSalaryItem < ActiveRecord::Base
           # Headers
           sheet.add_row [ salary_table.project.try(:corporation).try(:name) ], \
             height: 60, style: wrap_header_first
-          sheet.add_row [ salary_table.month_display_zh + " 工资表" ], \
+          sheet.add_row [ salary_table.month_display_zh + "   工资表" ], \
             height: 30, style: wrap_header_second
           sheet.add_row columns.map{|col| self.human_attribute_name(col)}, \
             height: 60, style: wrap_header_third
