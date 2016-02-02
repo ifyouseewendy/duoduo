f563b212e747f9c854a91aeb41d76dfbf9e8c710
wendi <ifyouseewendy@gmail.com>
2016-02-02 09:59:44 +0800

    guard_st: Update based on non full day st

--- app/admin/guard_salary_table.rb
+++ app/admin/guard_salary_table.rb
@@ -5,28 +5,99 @@ ActiveAdmin.register GuardSalaryTable do
     parent: I18n.t("activerecord.models.normal_business"),
     priority: 5
 
-  permit_params { resource_class.ordered_columns(without_base_keys: true, without_foreign_keys: false) }
+  breadcrumb do
+    crumbs = []
+
+    if params['q'].present?
+      if (ncid=params['q']['normal_corporation_id_eq']).present?
+        nc = NormalCorporation.where(id: ncid).first
+        if nc.present?
+          crumbs << link_to('合作单位', "/normal_corporations")
+          crumbs << link_to(nc.name, "/normal_corporations?q[id_eq]=#{nc.id}")
+        end
+      end
+    elsif params[:action] == 'show'
+      crumbs << link_to('保安工资表', "/guard_salary_tables")
+    end
+
+    crumbs
+  end
+
+  scope "全部" do |record|
+    record.all
+  end
+  scope "存档" do |record|
+    record.archive
+  end
+  scope "活动" do |record|
+    record.active
+  end
+
+  config.sort_order = 'start_date_desc'
 
   index do
     selectable_column
-    resource_class.ordered_columns(without_foreign_keys: true).map(&:to_sym).map do |field|
-      column field
+
+    column :start_date, sortable: :start_date do |obj|
+      obj.month
     end
-    column :corporation, sortable: ->(obj){ obj.corporation.name }
+    column :name
+    column :normal_corporation, sortable: :normal_corporation_id do |obj|
+      nc = obj.normal_corporation
+      link_to nc.name, normal_corporation_path(nc), target: '_blank'
+    end
+    column :status do |obj|
+      status_tag obj.status_i18n, (obj.active? ? :yes : :no)
+    end
+
+    column :remark
+
+    column :salary_items_display, sortable: :start_date do |obj|
+      ul do
+        li( link_to "工资条", "/guard_salary_tables/#{obj.id}/guard_salary_items", target: '_blank' )
+        li( link_to "导入", "/guard_salary_items/import_new?guard_salary_table_id=#{obj.id}", target: '_blank' )
+      end
+    end
+
+    column :attachment, sortable: :start_date do |obj|
+      ul do
+        if obj.lai_table.present?
+          li( link_to '来表', obj.lai_table.url )
+        end
+        if obj.daka_table.present?
+          li( link_to '打卡表', obj.daka_table.url )
+        end
+      end
+    end
 
     actions do |st|
-      text_node "&nbsp;|&nbsp;&nbsp;".html_safe
       # item "发票", "/invoices?utf8=✓&q%5Binvoicable_id_eq%5D=#{st.id}&invoicable_type%5D=#{st.class.name}&commit=过滤&order=id_desc", class: "member_link expand_table_action_width"
-      item "工资条", guard_salary_table_guard_salary_items_path(st), class: "member_link"
     end
   end
+
+  filter :start_date, as: :select, collection: -> { GuardSalaryTable.dates_as_filter }
+  filter :name
+  filter :normal_corporation, as: :select, collection: -> { NormalCorporation.as_filter }
+  filter :status, as: :check_boxes, collection: ->{ GuardSalaryTable.statuses_option(filter: true) }
+  preserve_default_filters!
+  # remove_filter :invoices
+  remove_filter :salary_items
+  remove_filter :lai_table
+  remove_filter :daka_table
+  remove_filter :activities
+
+  permit_params { resource_class.ordered_columns(without_base_keys: true, without_foreign_keys: false) }
 
   form do |f|
     f.semantic_errors(*f.object.errors.keys)
 
     f.inputs do
-      f.input :normal_corporation, as: :select
+      f.input :start_date, as: :datepicker, hint: '请选择月份第一天代表该月。例如，9月工资表请选择9月1号'
       f.input :name, as: :string
+      f.input :normal_corporation, as: :select, collection: -> { NormalCorporation.as_filter }.call
+      f.input :status, as: :radio, collection: ->{ GuardSalaryTable.statuses_option }.call
+      f.input :lai_table, as: :file
+      f.input :daka_table, as: :file
       f.input :remark, as: :text
     end
 
@@ -35,10 +106,101 @@ ActiveAdmin.register GuardSalaryTable do
 
   show do
     attributes_table do
-      GuardSalaryTable.ordered_columns(without_foreign_keys: true).map(&:to_sym).map do |field|
-        row field
+      row :start_date do |obj|
+        obj.month
       end
-      row :corporation
+      row :name
+      row :normal_corporation do |obj|
+        nc = obj.normal_corporation
+        link_to nc.name, normal_corporation_path(nc)
+      end
+      row :status do |obj|
+        status_tag obj.status_i18n, (obj.active? ? :yes : :no)
+      end
+
+      row :salary_items do |obj|
+        link_to "工资条", guard_salary_table_guard_salary_items_path(obj), target: '_blank'
+      end
+
+      row :lai_table do |obj|
+        link_to (obj.lai_table_identifier || '无'), (obj.lai_table.try(:url) || '#')
+      end
+      row :daka_table do |obj|
+        link_to (obj.daka_table_identifier || '无'), (obj.daka_table.try(:url) || '#')
+      end
+
+      row :remark
+      row :created_at
+      row :updated_at
+    end
+
+    active_admin_comments
+  end
+
+  # Batch actions
+  batch_action :batch_edit, form: ->{ GuardSalaryTable.batch_form_fields } do |ids|
+    inputs = JSON.parse(params['batch_action_inputs']).with_indifferent_access
+
+    failed = []
+    batch_action_collection.find(ids).each do |obj|
+      begin
+        obj.update_attributes!(inputs)
+      rescue => _
+        failed << "操作失败<编号#{obj.nest_index}>: #{obj.errors.full_messages.join(', ')}"
+      end
+    end
+
+    if failed.present?
+      redirect_to :back, alert: failed.join('; ')
+    else
+      redirect_to :back, notice: "成功更新 #{ids.count} 条记录"
+    end
+  end
+
+  member_action :audition_state do
+    st = GuardSalaryTable.find(params[:id])
+    audition = st.audition
+    display = st.audition_display
+
+    admin = current_active_admin_user.admin?
+    data = GuardSalaryTable::AUDITION_STAGE.reduce([]) do |ar, k|
+      ar << {
+        key: k,
+        value: audition[k.to_s],
+        display: display[k],
+        hide: !admin
+      }
+    end
+    render json: { status: :ok, data: data }
+  end
+
+  member_action :toggle_audition_state do
+    st = GuardSalaryTable.find(params[:id])
+    state = params[:state]
+
+    st.audition[state.to_s.strip] = current_active_admin_user.name
+    st.save!
+
+    render json: { status: :ok, message: '操作成功' }
+  end
+
+  member_action :update_remark do
+    st = GuardSalaryTable.find(params[:id])
+    st.remark = params[:remark]
+    st.save!
+    render json: { status: :ok, message: '操作成功' }
+  end
+
+  controller do
+    after_action :set_audition, only: :create
+
+    def scoped_collection
+      end_of_association_chain.includes(:normal_corporation)
+    end
+
+    def set_audition
+      resource.audition[:make_table] = current_active_admin_user.name
+      resource.save!
     end
   end
 end
--- app/models/guard_salary_table.rb
+++ app/models/guard_salary_table.rb
@@ -10,6 +10,15 @@ class GuardSalaryTable < ActiveRecord::Base
   has_many :guard_salary_items, dependent: :destroy
   # has_many :invoices, dependent: :destroy, as: :invoicable
 
+  mount_uploader :lai_table, Attachment
+  mount_uploader :daka_table, Attachment
+
+  enum status: [:active, :archive]
+
+  validates_presence_of :start_date
+
+  before_create :set_audition
+
   class << self
     def policy_class
       BusinessPolicy
@@ -23,28 +32,152 @@ class GuardSalaryTable < ActiveRecord::Base
 
       names
     end
+
+    def dates_as_filter
+      self.select(:start_date).distinct.order(start_date: :desc).map{|st| [st.month, st.start_date.to_s] }
+    end
+
+    def statuses_option(filter: false)
+      if filter
+        statuses.map{|k,v| [I18n.t("activerecord.attributes.#{self.name.underscore}.statuses.#{k}"), v]}
+      else
+        statuses.keys.map{|k| [I18n.t("activerecord.attributes.#{self.name.underscore}.statuses.#{k}"), k]}
+      end
+    end
+
+    def batch_form_fields
+      fields = [:remark]
+      hash = {
+        'status_状态' => [ ['活动', 'active'], ['存档', 'archive'] ],
+      }
+      fields.each{|k| hash[ "#{k}_#{human_attribute_name(k)}" ] = :text }
+      hash
+    end
   end
 
   def corporation
     normal_corporation
   end
 
-  def export_xlsx(options: {})
-    filename = "#{I18n.t("activerecord.models.guard_salary_table")}_#{Time.stamp}.xlsx"
+  def export_xlsx(view: nil, options: {})
+    staff_view = options['normal_staff_id_eq'].present?
+    if staff_view
+      collection = GuardSalaryItem.ransack(options).result
+      staff_name = collection.first.staff_name
+      sheet_name = staff_name
+      filename = "#{staff_name}_#{Time.stamp}.xlsx"
+    else
+      collection = salary_items.order(nest_index: :asc)
+      sheet_name = name
+      filename = filename_by(view: view)
+    end
+
     filepath = EXPORT_PATH.join filename
 
-    collection = guard_salary_items
     collection = collection.where(id: options[:selected]) if options[:selected].present?
 
-    columns = GuardSalaryItem.columns_based_on(options: options)
+    columns = present_fields(collection: collection, view: view, custom: options[:custom])
+
+    data_types = columns.reduce([]) do |ar, col|
+      if col == :staff_account
+        ar << :string
+      else
+        ar << nil
+      end
+    end
 
     Axlsx::Package.new do |p|
-      p.workbook.add_worksheet(name: name) do |sheet|
-        sheet.add_row columns.map{|col| GuardSalaryItem.human_attribute_name(col)}
+      wb = p.workbook
+      wrap_header_first = wb.styles.add_style({
+        font_name: "新宋体",
+        alignment: {horizontal: :center, vertical: :center, wrap_text: true},
+        b: true,
+        sz: 18
+      })
+      wrap_header_second = wb.styles.add_style({
+        font_name: "新宋体",
+        alignment: {horizontal: :center, vertical: :center, wrap_text: true},
+        height: 30,
+        b: true,
+        sz: 12
+      })
+      wrap_header_third = wb.styles.add_style({
+        font_name: "新宋体",
+        alignment: {horizontal: :center, vertical: :center, wrap_text: true},
+        border: {style: :thin, color: '00'},
+        height: 60,
+        sz: 10
+      })
+      wrap_text = wb.styles.add_style({
+        font_name: "新宋体",
+        alignment: {horizontal: :center, vertical: :center, wrap_text: true},
+        border: {style: :thin, color: '00'},
+        height: 30,
+        sz: 10
+      })
+      wrap_float_text = wb.styles.add_style({
+        font_name: "新宋体",
+        alignment: {horizontal: :right, vertical: :center, wrap_text: true},
+        border: {style: :thin, color: '00'},
+        height: 30,
+        format_code: '0.00',
+        sz: 10
+      })
+      margins = {left: 0.1, right: 0.1, top: 0.1, bottom: 0.1}
+      setup = {fit_to_width: 1, orientation: :landscape}
 
-        collection.each do |item|
-          sheet.add_row columns.map{|col| item.send(col)}
+      wb.add_worksheet(name: sheet_name, page_margins: margins, page_setup: setup) do |sheet|
+        # Fit to page printing
+        # sheet.page_setup.fit_to :width => 1
+
+        # Headers
+        if staff_view
+          sheet.add_row columns.map{|col| GuardSalaryItem.human_attribute_name(col)}, \
+            height: 60, b:true, style: wrap_header_third
+        else
+          sheet.add_row [ corporation.full_name || corporation.name ], \
+            height: 60, b:true, sz: 16, style: wrap_header_first
+          sheet.add_row [ start_date.to_s ], \
+            height: 30, b:true, style: wrap_header_second
+          sheet.add_row columns.map{|col| GuardSalaryItem.human_attribute_name(col)}, \
+            height: 60, b:true, style: wrap_header_third
+
+          end_col = ('A'.ord + columns.count - 1).chr
+          sheet.merge_cells("A1:#{end_col}1")
+          sheet.merge_cells("A2:#{end_col}2")
         end
+
+        # Content
+        collection.each do |item|
+          data = columns.map do |col|
+            item.send(col).to_s
+          end
+          sheet.add_row data, style: ([wrap_text]*3 + [wrap_float_text]*(columns.count-3)), height: 30, types: data_types
+        end
+
+        # Sum row
+        stats = columns.reduce([]) do |ar, col|
+          if GuardSalaryItem.sum_fields.include?(col)
+            ar << collection.sum(col).to_f.round(2)
+          else
+            ar << nil
+          end
+        end
+        stats[0] = '合计'
+        sheet.add_row stats, style: ([wrap_text]*1 + [wrap_float_text]*(columns.count-1)), height: 30
+
+        end_rol = 3 + collection.count + 1
+        sheet.merge_cells("A#{end_rol}:C#{end_rol}")
+
+        # Footer
+        sheet.add_row []
+        sheet.add_row [ ['经理：', '财务：', '部门经理:', '审核：', '复核：', '制表：'].join('                    ') ]
+
+        # Set First column width
+        # widths = [3, 20, 8] + Array.new(columns.count+1-3){8}
+        sheet.column_widths 5
+
+        wb.add_defined_name("'#{sheet_name}'!$1:$3", :local_sheet_id => sheet.index, :name => '_xlnm.Print_Titles') 
       end
       p.serialize(filepath.to_s)
     end
@@ -52,8 +185,62 @@ class GuardSalaryTable < ActiveRecord::Base
     filepath
   end
 
+  def filename_by(view: nil)
+    filename = \
+      case view.to_s
+      when "proof"    then "凭证工资表（帐用）"
+      when "card"     then "打卡表"
+      when "custom"   then "自定义工资表"
+      when "whole"    then "全部字段工资表"
+      else "基础工资表"
+      end
+    "#{corporation.name}_#{name}_#{filename}_#{Time.stamp}.xlsx"
+  end
+
+  def status_i18n
+    I18n.t("activerecord.attributes.#{self.class.name.underscore}.statuses.#{status}")
+  end
+
+  def month
+    start_date.strftime("%Y年%m月")
+  end
+
+  def available_nest_index
+    ( salary_items.reorder(nest_index: :desc).first.try(:nest_index) || 0 ) + 1
+  end
+
+  def present_fields(collection:, view: , custom: )
+    fields = GuardSalaryItem.columns_based_on(view: view, custom: custom)
+    fields.select do |key|
+      collection.map{|obj| obj.send(key)}.any? do |val|
+        if Numeric === val
+          val.nonzero?
+        else
+          val.present?
+        end
+      end
+    end
+  end
+
   def display_name
     name
   end
 
+  AUDITION_STAGE = [:make_table, :audit_first, :audit_second, :audit_finance]
+  def set_audition
+    AUDITION_STAGE.each{|k| self.audition[k] = nil}
+  end
+
+  def audition_display
+    {
+      make_table: '制表',
+      audit_first: '复核',
+      audit_second: '审核',
+      audit_finance: '财务',
+    }
+  end
+
+  def salary_items
+    guard_salary_items
+  end
 end
