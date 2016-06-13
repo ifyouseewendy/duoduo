407969fec124751c2d57f1a0858fee2ac9650b64
wendi <ifyouseewendy@gmail.com>
2016-06-13 15:18:09 +0800

    sub_company_policy: Rm default visible policy

--- app/policies/sub_company_policy.rb
+++ app/policies/sub_company_policy.rb
@@ -1,11 +1,7 @@
 class SubCompanyPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if user.visible_sub_company_ids.blank?
-        scope
-      else
-        scope.where(id: user.visible_sub_company_ids)
-      end
+      scope.where(id: user.visible_sub_company_ids)
     end
   end
 end
